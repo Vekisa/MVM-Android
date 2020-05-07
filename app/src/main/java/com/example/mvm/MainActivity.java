795b0130c19ae494e94eb21ad41e9c5201086f91
levarsky <mlevarski@gmail.com>
2020-05-07 17:35:19 +0200

    SignIn and Registration activity

--- app/src/main/java/com/example/mvm/MainActivity.java
+++ app/src/main/java/com/example/mvm/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLoginClick(View v) {
+        Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void onRegClick(View v) {
+        Intent regIntent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(regIntent);
+    }
+
+
 }
