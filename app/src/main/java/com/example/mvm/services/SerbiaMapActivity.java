bba58f9867f520a83e02fa318713b0c78be54337
micamat <matijevicmilica96@gmail.com>
2020-06-01 19:43:35 +0200

    pull

---
+++ app/src/main/java/com/example/mvm/services/SerbiaMapActivity.java
@@ -0,0 +1,14 @@
+package com.example.mvm.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SerbiaMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serbia_map);
+    }
+}
